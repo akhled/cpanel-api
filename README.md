26a0ba7b89196e2724033765bf57106eb41b1aa6
Amro Khaled <wshcylinder@gmail.com>
2022-01-06 16:55:36 +0200

    :tada: Init

---
+++ README.md
@@ -0,0 +1,1 @@
+# CPANEL API
---
+++ src/Providers/CPanelAPIServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Akhaled\CPanelAPI\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CPanelAPIServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
